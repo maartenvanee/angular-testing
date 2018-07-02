d59be44e7781b57ddcea36b0d64fa3cdc0c8d799
Maarten van Ee <maartenvanee@gmail.com>
2018-07-02 15:43:20 +0200

    added functionality and test functions

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,27 @@
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to testapp!');
-  });
+    it('should display Title', () => {
+        page.navigateTo();
+        expect(page.getTitleText()).toEqual('Angular Testing');
+    });
+
+    it('should click three times and reset with matching points', () => {
+        page.navigateTo();
+        expect(page.getPoints()).toBe('1');
+
+        page.getPlusButton().click();
+        page.getPlusButton().click();
+        page.getPlusButton().click();
+
+        expect(page.getPoints()).toBe('4');
+        page.getResetButton().click();
+        expect(page.getPoints()).toBe('0');
+    });
 });
